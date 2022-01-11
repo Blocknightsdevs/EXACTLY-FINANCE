45eed509bfca0ed54885cf27eb8a48d46e1faa3e
rodrigogk87 <88176133+Blocknightsdevs@users.noreply.github.com>
2022-01-11 15:54:53 -0300

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,45 +6,14 @@
 const hre = require("hardhat");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
-
-  // We get the contract to deploy
+  
   const Rewards = await hre.ethers.getContractFactory("Rewards");
   
   const accounts = await ethers.getSigners()
-  const account = await accounts[0].getAddress()
-  const rewards = await Rewards.deploy(account);
+  const team = await accounts[1].getAddress()
+  const rewards = await Rewards.deploy(team);
 
   await rewards.deployed();
-
-  //send 1eth as deposit
-  
-  let balance = await accounts[0].getBalance();
-  console.log("balance bd", balance);
-
-  await rewards.deposit({value: ethers.utils.parseEther("1.0")});
-  
-   balance = await accounts[0].getBalance();
-  console.log("balance ad", balance);
-
-  //send 1 eth as reward
-  await rewards.depositReward({value: ethers.utils.parseEther("1.0")});
-
-  
-   balance = await accounts[0].getBalance();
-  console.log("balance ar", balance);
-
-  //wirthdraw
-  await rewards.wirthdraw();
-
-  
-   balance = await accounts[0].getBalance();
-  console.log("balance aw", balance);
 
   console.log("rewards deployed to:", rewards.address);
 }
